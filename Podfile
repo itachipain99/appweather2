13095b0bb1c7d122d62986d4386d6b2ebb7de1ff
hieu <itachipain11@gmail.com>
2019-09-29 01:23:44 +0700

    new repo

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Baitapjson' do
+pod 'SwiftyJSON', '~> 4.0'
+end
---
+++ Pods/Target Support Files/Pods-Baitapjson/Pods-Baitapjson-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Baitapjson : NSObject
+@end
+@implementation PodsDummy_Pods_Baitapjson
+@end
